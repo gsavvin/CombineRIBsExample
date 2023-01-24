6498328d2c45edc1f4e5a49746183260c7bbcc13
Gennady Savvin <32610253+gsavvin@users.noreply.github.com>
2023-01-24 09:21:19 +0300

    Initial commit

---
+++ CombineRIBsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineRIBsExample
+//
+//  Created by Геннадий Саввин on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
